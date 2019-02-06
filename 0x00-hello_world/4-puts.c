85a7aacb6ac0d318f187078eba1ceb6b77d93c02
miguelfajardoc <775@holbertonschool.com>
2019-02-06 23:54:30 +0000

    printing with C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char st[] = "\"Programming is like building a multilingual puzzle";
+puts(st);
+return (0);
+}

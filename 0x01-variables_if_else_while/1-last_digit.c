dbc21266c886683442170d4a5fec08646ad4348f
miguelfajardoc <775@holbertonschool.com>
2019-02-07 22:06:11 +0000

    fixing first

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ *
+ * Description: this program print if a number is positive or negative
+ */
 int main(void)
 {
 	int n;
+	int module;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	char *result;
-
-	if (n > 5)
-		result = "and is greater than 5";
-	else if (n == 0)
-		result = "and is 0";
+	module = n%10;
+	if (module > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, module);
+	else if (module == 0)
+		printf("Last digit of %d is %d and is 0\n", n, module);
 	else
-		result = "and is less than 6 and not 0";
-
-	printf("Last digit of %d is %s\n", n, result);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, module);
 	return (0);
 }

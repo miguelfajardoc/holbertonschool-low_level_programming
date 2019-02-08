d6c7db8c175cc3a75eba8bf950e320dbc3377d89
miguelfajardoc <775@holbertonschool.com>
2019-02-08 00:59:53 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ *
+ * Description: this program print the alphabet
+ */
+int main(void)
+{
+
+	char a;
+
+	for (a = '1'; a <= '9'; ++a)
+	{
+		putchar(a);
+
+	}
+	for (a = 'a'; a <= 'f'; ++a)
+	{
+		putchar(a);
+
+	}
+	putchar('\n');
+	return (0);
+}

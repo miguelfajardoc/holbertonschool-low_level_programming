377eeb892bf625f605f043cfe2fc69a42a9ccfb4
miguelfajardoc <775@holbertonschool.com>
2019-02-08 00:47:28 +0000

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ *
+ * Description: this program print the alphabet
+ */
+int main(void)
+{
+
+	char a;
+
+	for (a = 'z'; a >= 'a'; --a)
+	{
+		putchar(a);
+
+	}
+	putchar('\n');
+	return (0);
+}

6fdff0fd89987b56b0f0ba6c9b5ac6a3f4e3ec3a
miguelfajardoc <775@holbertonschool.com>
2019-02-12 21:19:05 +0000

    10 alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet_10x - this function print the alphabet in lowercase 10 times
+ * print_alphabet_x10 - this function print the alphabet in lowercase 10 times
  * Description: using a ford nested
  */
 void print_alphabet_x10(void)
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int letra;
 	int i;
 
-	for (i = 0; i< 10 ; i++)
+	for (i = 0; i < 10 ; i++)
 	{
 		for (letra = 'a'; letra <= 'z' ; letra++)
 		{

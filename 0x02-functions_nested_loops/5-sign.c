c43748c61644f51af56c5e577cb817d7a4c553ae
miguelfajardoc <775@holbertonschool.com>
2019-02-12 23:05:18 +0000

    the sign of the number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * sign - check if a numbeer is positive or negative, print the sign.
+ * @n: the number to check
+ * Return: 1 if the number is positive an 0 if is negative.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		n = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		n = 0;
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+		n = -1;
+	}
+	return (n);
+}

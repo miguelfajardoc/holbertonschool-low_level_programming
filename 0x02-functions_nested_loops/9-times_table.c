43ff872a5ff78d6d471254046a531897f9fce366
miguelfajardoc <775@holbertonschool.com>
2019-02-13 01:05:44 +0000

    9 times mult table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * times_table - print mult table
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int n;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			n = i * j;
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (i != 9)
+				_putchar(',');
+		}
+			_putchar('\n');
+	}
+}

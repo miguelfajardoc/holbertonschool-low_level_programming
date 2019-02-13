a92e11c318d6b7eba534067ef4ca0583ed06c8f4
miguelfajardoc <775@holbertonschool.com>
2019-02-13 19:22:08 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98 ; i--)
 		{
-
+			if (i == 98 && i == n)
+			{
+				printf("%d\n", i);
+			}
 			if (i == 98)
 			{
-				printf("%d \n", i);
+				printf(" %d\n", i);
 			}
 			else if (i == n)
 				printf("%d,", i);
@@ -29,9 +32,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98 ; i++)
 		{
-			if (i == 98)
+			if (i == 98 && i == n)
 			{
-				printf("%d \n", i);
+				printf("%d\n", i);
+			}
+			else if (i == 98)
+			{
+				printf(" %d\n", i);
 			}
 			else if (i == n)
 				printf("%d,", i);

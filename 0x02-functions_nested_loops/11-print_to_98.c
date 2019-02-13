468539b9b4f8bdd88588d53a3ecae2ebd4d9982a
miguelfajardoc <775@holbertonschool.com>
2019-02-13 19:15:14 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print the numbers until reatch 98
+ * @n: the start number
+ * Return: none
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98 ; i--)
+		{
+
+			if (i == 98)
+			{
+				printf("%d \n", i);
+			}
+			else if (i == n)
+				printf("%d,", i);
+			else
+				printf(" %d,", i);
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98 ; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d \n", i);
+			}
+			else if (i == n)
+				printf("%d,", i);
+			else
+				printf(" %d,", i);
+		}
+	}
+}

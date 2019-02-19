7e3f3b56112c30ffb345f8c2fe3ca39d5497cb35
miguelfajardoc <775@holbertonschool.com>
2019-02-19 21:48:43 +0000

    swap

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * swap_int - swap the assignement of two vars.
+ *
+ * @a: The first variable to swap
+ * @b: The secont variable to swap
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+	x = *a;
+	*a = *b;
+	*b = x;
+}

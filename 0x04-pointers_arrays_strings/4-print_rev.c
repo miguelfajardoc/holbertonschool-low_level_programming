d51a72ef550f4e800068d6f4d6d259d003ddb7e3
miguelfajardoc <775@holbertonschool.com>
2019-02-19 23:36:20 +0000

    tnirp

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse.
+ *
+ * @s: the string to print.
+ *
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	while (l >= 0)
+	{
+		_putchar(*s);
+		s--;
+		l--;
+	}
+	_putchar('\n');
+}

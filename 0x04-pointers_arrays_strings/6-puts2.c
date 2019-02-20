987b61d8dd81fd9c90df7beed0e2cf2707307ee4
miguelfajardoc <775@holbertonschool.com>
2019-02-20 03:41:47 +0000

    2 by 2

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - prints a string one caracter of two, starting with the first.
+ *
+ * @str: the string to print.
+ *
+ */
+void puts2(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}

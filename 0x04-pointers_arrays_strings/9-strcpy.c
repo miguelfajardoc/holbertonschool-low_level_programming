261c9f37fd4227aee1008421ccbe87733561176f
miguelfajardoc <775@holbertonschool.com>
2019-02-21 00:02:16 +0000

    cpy

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 
 	while (p)
 	{
-		if ( i != 0 && *(dest + (i - 1)) == '\0')
+		if ( i != 0 && *(src + (i)) == '\0')
 		{
 			*(dest + i) = *(src + i);
 			p--;

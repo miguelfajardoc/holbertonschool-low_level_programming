d82b82479cb4ca99c14130f55d00e5411cf0aebe
miguelfajardoc <775@holbertonschool.com>
2019-02-21 23:32:24 +0000

    reversearray

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse array of ints.
+ *
+ * @a: the array to reverse.
+ * @n: the number of elements in the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int t;
+	int m;
+	int c = 0;
+	n--;
+	m = n / 2;
+	for (c = 0 ; c <= m; c++)
+	{
+		t = a[c];
+		a[c] = a[n];
+		a[n] = t;
+		n--;
+	}
+}

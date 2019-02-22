3bc66202e3921ea94a4645b2bac526464dfebeb8
miguelfajardoc <775@holbertonschool.com>
2019-02-22 23:51:55 +0000

    rot13

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -9,19 +9,21 @@ char *rot13(char *s)
 {
 	int i = 0;
 	int j = 0;
-	int lto[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
-	int ltd[] = {4, 3, 0, 7, 1, 4, 3, 0, 7, 1};
+	char roto[] = "abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ";
+	char rotd[] = "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM";
 
 	while (*(s + i) != '\0')
 	{
-		j = 0;
-		while (s[i] != lto[j] && *(lto + j) != '\0')
+		if ( (s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' &&
+						      s[i] >= 'A'))
 		{
-			j++;
+			j = 0;
+			while (s[i] != roto[j])
+			{
+				j++;
+			}
+			s[i] = rotd[j];
 		}
-		printf("%d\n", j);
-		if (j < 10)
-			s[i] = ltd[j];
 		i++;
 	}
 	return (s);

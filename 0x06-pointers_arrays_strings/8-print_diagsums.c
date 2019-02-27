2a863eadcca9318a4900b1decdd46102ed3402d9
miguelfajardoc <775@holbertonschool.com>
2019-02-27 00:04:54 +0000

    print sum strange matrix

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - sum the two diagonals of a square matrix.
+ * @a: pointer of the matrix
+ * @size: the size of the matrix
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0 ; i < (size * size) ; i += size + 1)
+	{
+		sum += a[i];
+	}
+	printf("%d, ", sum);
+	i = size - 1;
+	sum = 0;
+	for (i = 0 ; i + 1 < (size * size) ; i += size - 1)
+	{
+		sum += a[i];
+	}
+	printf("%d\n", sum);
+
+}

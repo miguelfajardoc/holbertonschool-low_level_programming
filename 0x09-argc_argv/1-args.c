1bddadcbb85e43d19e476e4b1f99d4178772e09e
miguelfajardoc <775@holbertonschool.com>
2019-03-03 22:36:46 +0000

    c, c, c. the number of the args

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the number of the arguments passed
+ * @argc: the amount of args.
+ * @argv: the array that store the arguments.
+ * Return: always 0.
+ */
+int main(int argc, char __attribute__((__unused__))*argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

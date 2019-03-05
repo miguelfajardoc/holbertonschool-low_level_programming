d47498bf1b439d3edff5e8aa6b0b2cd3764352f9
miguelfajardoc <775@holbertonschool.com>
2019-03-05 23:49:37 +0000

    null evader

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * size_string - calculate the size of the string
+ * @str: the string that passed
+ *
+ *Return: the size of the string, if the string is null, return 0;
+ */
+int size_string(char *str)
+{
+	int size;
+
+	if (str == NULL)
+		return (1);
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	size++;
+	return (size);
+}
+/**
  * str_concat - concatenate two functions
  * @s1: the first string to concatenate
  * @s2: the second string to concatenate
- * @str: the string that passed
  * Return: the pointer to strings concatenated. If NULL is passed, treat it as
  * empty string. the function should return NULL on failure.
  */
-int size_string(char *str);
+
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
@@ -27,28 +43,12 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < (sz1 - 1); i++)
 		p[i] = s1[i];
-	for (i = (sz1 - 1); i < szt; i++)
+	for (i = (sz1 - 1); i < (szt - 1); i++)
 	{
 		p[i] = s2[j];
 		j++;
 	}
+	p[i] = '\0';
 
 	return (p);
-}
-/**
- * size_string - calculate the size of the string
- * @str: the string that passed
- *
- *Return: the size of the string, if the string is null, return 0;
- */
-int size_string(char *str)
-{
-	int size;
-
-	if (str == NULL)
-		return (1);
-	for (size = 0; str[size] != '\0'; size++)
-		;
-	size++;
-	return (size);
 }

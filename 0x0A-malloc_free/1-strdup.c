9041c54aad5b10113f07e92269be345d20c3843a
miguelfajardoc <775@holbertonschool.com>
2019-03-05 21:26:48 +0000

    string string

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - create a copy of string recived as a parameter
+ * @str: the string to copy
+ *
+ * Return: the pointer that point to the array created.
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	p = malloc(sizeof(char) * sizeof(str));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i <= sizeof(str); i++)
+		p[i] = str[i];
+
+	return (p);
+}

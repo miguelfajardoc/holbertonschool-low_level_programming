c895c0e478c48efab8f393384c3a3f9a9de452ce
miguelfajardoc <775@holbertonschool.com>
2019-03-05 23:34:40 +0000

    string + string

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - concatenate two functions
+ * @s1: the first string to concatenate
+ * @s2: the second string to concatenate
+ * @str: the string that passed
+ * Return: the pointer to strings concatenated. If NULL is passed, treat it as
+ * empty string. the function should return NULL on failure.
+ */
+int size_string(char *str);
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i;
+	int j = 0;
+	int sz1;
+	int sz2;
+	int szt;
+
+	sz1 = size_string(s1);
+	sz2 = size_string(s2);
+	szt = sz1 - 1 + sz2;
+	p = malloc(sizeof(char) * szt);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (sz1 - 1); i++)
+		p[i] = s1[i];
+	for (i = (sz1 - 1); i < szt; i++)
+	{
+		p[i] = s2[j];
+		j++;
+	}
+
+	return (p);
+}
+/**
+ * size_string - calculate the size of the string
+ * @str: the string that passed
+ *
+ *Return: the size of the string, if the string is null, return 0;
+ */
+int size_string(char *str)
+{
+	int size;
+
+	if (str == NULL)
+		return (1);
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	size++;
+	return (size);
+}

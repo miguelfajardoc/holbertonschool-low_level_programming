5ed0a0b3f4ca6f396ee9bbb359578cd65b3c6531
miguelfajardoc <775@holbertonschool.com>
2019-03-05 20:58:58 +0000

    create maloc!

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	p = malloc(sizeof(char) * size);
-	if (p == null)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;

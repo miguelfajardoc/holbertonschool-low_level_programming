f8a7d490007bf6b147c204ac62ae2502e78a8a43
miguelfajardoc <775@holbertonschool.com>
2019-03-06 02:58:08 +0000

    matrix zero

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - give a pointer that locate 2 dimensional array
+ * @width: the width of the grid
+ * @height: the height of the grid
+ * Return: the pointer to the grid. The grid should be start in 0. If width or
+ * height is 0 or negative, return NULL.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int j = 0;
+	int **d;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	d = malloc (sizeof(int*) * height);
+	if (d == NULL)
+		return (NULL);
+	while (j < width)
+	{
+		*(d + j) = malloc (sizeof(int) * width);
+		if (*(d + j) == NULL)
+			return (NULL);
+		j++;
+	}
+	j = 0;
+	while (i < height)
+	{
+		while (j < width)
+		{
+			d[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return (d);
+}

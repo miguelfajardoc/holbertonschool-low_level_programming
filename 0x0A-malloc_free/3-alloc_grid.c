9407adfd4ae80f21855e72cd40c36c02396c99f5
miguelfajardoc <775@holbertonschool.com>
2019-03-06 19:46:44 +0000

    free matrix

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,8 @@ int **alloc_grid(int width, int height)
 		*(d + j) = malloc(sizeof(int) * width);
 		if (*(d + j) == NULL)
 		{
-			free(d);
+			for ( ; j >= 0; j--)
+				free(*(d + j));
 			return (NULL);
 		}
 		j++;

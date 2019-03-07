431405c29c033bbad4a33dfa743e3b1c52521536
miguelfajardoc <775@holbertonschool.com>
2019-03-07 19:51:55 +0000

    README

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,13 @@
 /**
  * malloc_checked - alloc a memory with malloc property
  * @b: the var that bring the amount of memory to allocate
- *
+ * Return: the void pointer that the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *s = malloc(b);
+
 	if (s == NULL)
 		exit(98);
-	return (s);
+	return ((void *)s);
 }

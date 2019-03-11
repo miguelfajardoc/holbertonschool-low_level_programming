f8c4d9e29d7e42476c5039a5381b148f53813472
miguelfajardoc <775@holbertonschool.com>
2019-03-11 22:39:22 +0000

    caisy

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - struct for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

0012a21284f3d8d85640d6f53cbe81ac23143dd9
miguelfajardoc <775@holbertonschool.com>
2019-03-12 01:59:48 +0000

    new doggie

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * size_string - calculate the size of the string
+ * @str: the string that passed
+ *
+ *Return: the size of the string, within null, if the string is null, return 0;
+ */
+char *_strdup(char *str);
+int size_string(char *str)
+{
+	int size;
+
+	if (str == NULL)
+		return (1);
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	size++;
+	return (size);
+}
+/**
+ * _strdup - create a copy of string recived as a parameter
+ * @str: the string to copy
+ *
+ * Return: the pointer that point to the array created.
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+	size = size_string(str);
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		free (str);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		p[i] = str[i];
+
+	return (p);
+}
+/**
+ * init_dog - initialize dog.
+ * @d: the structure of the dog d
+ * @name: pointer to the name
+ * @age: the age
+ * @owner: the owner
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *nomb;
+	char *own;
+
+	nomb = _strdup(name);
+	if (nomb == NULL)
+		return (NULL);
+	own = _strdup(owner);
+	if (own == NULL)
+		return (NULL);
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+	{
+		free (new);
+		return (NULL);
+	}
+
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
+}

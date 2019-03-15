00cb81bfce0afd6c7df994dd245ea226e100fa37
miguelfajardoc <775@holbertonschool.com>
2019-03-15 21:40:49 +0000

    excercise 2

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - print numbers with a separator
+ * @separator: the separator that separates the numbers
+ * @n: the number of args.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vlist;
+	unsigned int i;
+
+	va_start(vlist, n);
+	if (separator == NULL)
+	{
+		for (i = 0; i < n - 1; i++)
+		printf("%d", va_arg(vlist, int));
+	}
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(vlist, int), separator);
+		printf("%d", va_arg(vlist,int));
+	}
+	va_end(vlist);
+	printf("\n");
+}
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef VARIADICSF
 #define VARIADICSF
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif

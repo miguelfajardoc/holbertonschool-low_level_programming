27961090c7aca8d688dac41f865c3b8bfc1161de
miguelfajardoc <775@holbertonschool.com>
2019-03-22 01:32:45 +0000

    advance

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include "lists.h"
+/**
+ * StartFunction - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void StartFunction(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon");
-	printf("my back!\n");
+	printf(" my back!\n");
 }

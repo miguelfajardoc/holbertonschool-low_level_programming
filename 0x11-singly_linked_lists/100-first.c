74805b44e1fd03ca7aa58732935c597bbceacb53
miguelfajardoc <775@holbertonschool.com>
2019-03-22 01:23:21 +0000

    advance

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,5 +2,6 @@
 #include "lists.h"
 void StartFunction(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon");
+	printf("my back!\n");
 }

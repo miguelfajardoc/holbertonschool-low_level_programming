c060df2af4dcd591b1cc91033a975ccee400ae60
miguelfajardoc <775@holbertonschool.com>
2019-03-23 02:39:47 +0000

    Hello holberton with printf with asm !

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+	global	main
+	extern	printf
+
+	section	.text
+
+main:
+	push	rbp
+	mov	rbp, rsp
+	mov	rdi, message
+	call	printf
+
+	leave
+	ret
+
+	section	.data
+	message	db	"Hello, Holberton", 10, 0

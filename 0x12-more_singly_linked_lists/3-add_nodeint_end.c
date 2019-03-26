dffecaa4da26c78976f674dfd6cb2a4784bc1f9f
miguelfajardoc <775@holbertonschool.com>
2019-03-26 21:17:09 +0000

    add a member to the end of a list

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - add a node to the end of the list
+ * @head: the pointer to the list
+ * @n: the data of the list
+ * Return: the number of nodes
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp;
+	listint_t *last;
+
+	last = *head;
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = temp;
+	return (*head);
+}

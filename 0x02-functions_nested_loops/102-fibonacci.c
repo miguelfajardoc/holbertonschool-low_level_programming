6d938b848cf10a1a380ca364260f342e1cac1a3f
miguelfajardoc <775@holbertonschool.com>
2019-03-26 02:44:48 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print the 50 fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int i = 0;
+	long int x = 1;
+	long int y = 0;
+	long int n = 0;
+
+	printf("%ld, ", x);
+	i = 1;
+	while (i < 50)
+	{
+		n = x + y;
+		printf("%ld", n);
+		y = x;
+		x = n;
+		i++;
+
+		if (i + 1 < 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}

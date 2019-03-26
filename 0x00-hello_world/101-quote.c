bcee55efd70c083c837e65b169f172d8d37d20bb
miguelfajardoc <775@holbertonschool.com>
2019-03-26 01:10:11 +0000

    advance quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
-int main()
+/**
+ * main - write a quote in the standar output
+ *
+ * Return: always 1.
+ */
+int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      60);
+	      59);
 	return (1);
 }

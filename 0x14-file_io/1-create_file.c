64778da640c669f2eb8170167811528c74e33bba
miguelfajardoc <775@holbertonschool.com>
2019-04-03 02:14:23 +0000

    create file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * create_file - create or rewrite a file with a content
+ * @filename: the name of the file
+ * @text_content: the text to create qhe file
+ * Return: Always the amount of letters printed.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int state;
+	int f_des;
+
+	if (filename == NULL)
+		return (-1);
+	f_des = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (f_des < 0)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	state = write(f_des, text_content, (_strlen(text_content) + 1));
+	if (state < 0)
+		return (-1);
+	return (1);
+
+}
+/**
+ * _strlen - find the size of a string.
+ *
+ * @s: the string to count
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; *s != 0; s++)
+		l++;
+	return (l);
+}

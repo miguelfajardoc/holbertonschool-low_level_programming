992ee901b0f10a7357296fcffcdff5c1cf30f06f
miguelfajardoc <775@holbertonschool.com>
2019-04-06 01:54:23 +0000

    fix

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -42,24 +42,24 @@ int main(int ac, char **av)
 		buff = read(fd_from, buffer, 1024);
 		if (buff == -1)
 		{
-			dprintf(2, "Error: Can't read form %s\n", av[1]);
+			dprintf(2, "Error: Can't read from %s\n", av[1]);
 			exit(98);
 		}
 		if (write(fd_to, buffer, buff) < 0)
 		{
-			dprintf(2, "Error: Can't write form %s\n", av[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
 	if (close(fd_from) > 0)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(99);
+		exit(100);
 	}
 	if (close(fd_to) > 0)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(99);
+		exit(100);
 	}
 	return (0);
 }

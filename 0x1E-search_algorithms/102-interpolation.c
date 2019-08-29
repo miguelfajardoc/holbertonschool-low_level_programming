3990ac3d1716a46a8278dad024d63e7cba83e95d
miguelfajardoc <775@holbertonschool.com>
2019-08-29 21:11:10 +0000

    fix when is null

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,6 +14,9 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t high = size - 1;
 	size_t pos;
 
+	if (array == NULL)
+		return (-1);
+
 	while (low <= high)
 	{
 		if (low == high)
